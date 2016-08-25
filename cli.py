94075c0bbc348a9df7350982f5b20b7f54875217
Riley Williams <riley.will1@gmail.com>
2016-08-25 18:44:42 +0100

    Updated cli.py, added -t and -s arguments for use with tiling

--- cli.py
+++ cli.py
@@ -4,18 +4,20 @@ import argparse
 from pycovjson.write import Writer
 
 parser = argparse.ArgumentParser(description='Convert Scientific Data Formats into CovJSON.')
-parser.add_argument('-i', '--input', dest='inputfile', help='Name of input file')
+parser.add_argument('-i', '--input', dest='inputfile', help='Name of input file', required=True)
 parser.add_argument('-o', '--output', dest='outputfile', help='Name and location of output file')
-parser.add_argument('-t', dest='tiled', help='Apply tiling')
+parser.add_argument('-t', '--tiled', action='store_true', help='Apply tiling')
+parser.add_argument('-s', '--shape', nargs='+', help='Tile shape, list', type=int)
 parser.add_argument('-v',dest='variable' ,help='Variable to populate coverage with')
-parser.add_argument('-r', dest='range_type', help='Range Type. E.g TiledNdArray or NdArray')
 args = parser.parse_args()
 inputfile = args.inputfile
 outputfile = args.outputfile
 variable = args.variable
-range_type = args.range_type
-
-Writer(outputfile, inputfile, [variable], range_type).write()
+tiled = args.tiled
+tile_shape = args.shape
+print(tile_shape)
+file_path = 'pycovjson/melodies_lc-latlon.nc'
+Writer(outputfile, inputfile, [variable], tiled=tiled, tile_shape=tile_shape).write()
 
 
 
