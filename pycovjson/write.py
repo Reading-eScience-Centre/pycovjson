da303b627ba7f3f01fa87adadff313f324bcb980
Riley Williams <riley.will1@gmail.com>
2016-08-29 20:28:48 +0100

    Restructured code. Removed redundancy

--- pycovjson/write.py
+++ pycovjson/write.py
@@ -6,8 +6,17 @@ import time, json, uuid
 
 
 class Writer(object):
+    """Writer class"""
 
     def __init__(self, output_name: object, dataset_path: object, vars_to_write: object, tiled=False, tile_shape=[] ) -> object:
+        """
+        Writer class constructor
+        :param output_name: Name of output file
+        :param dataset_path: Path to dataset
+        :param vars_to_write: List of variables to write
+        :param tiled: Boolean value (default False)
+        :param tile_shape: List containing shape of tiles
+        """
         self.output_name = output_name
         self.dataset_path = dataset_path
         self.tile_shape = tile_shape
@@ -34,6 +43,9 @@ class Writer(object):
 
 
     def write(self):
+        """
+        Writes coverageJSON object to disk
+        """
 
         coverage = self._construct_coverage()
         self._save_covjson(coverage, self.output_name)
@@ -41,14 +53,20 @@ class Writer(object):
 
 
     def _construct_coverage(self):
+        """
+        Constructs Coverage object from constituent parts
+        :return: coverage object
+        """
         coverage = Coverage(self._construct_domain(),self._construct_range(), self._construct_params(), self._construct_refs()).to_dict()
         return coverage
 
     def _construct_domain(self):
+        """
+        Constructs Domain object, populates with values
+        :return: domain object
+        """
 
         domain_type = 'Grid'
-        self.vars_to_write[0]
-
         x_values = self.Reader.get_x().flatten().tolist()
         y_values = self.Reader.get_y().flatten().tolist()
         t_values = []
@@ -57,7 +75,6 @@ class Writer(object):
         if 't' in self.axis_list:
 
             t_values = self.Reader.get_t()
-            print(t_values)
 
         if 'z' in self.axis_list:
 
@@ -70,13 +87,16 @@ class Writer(object):
         return domain
 
     def _construct_params(self):
+        """
+        Construct parameters
+        :return: Parameter object
+        """
         for variable in self.vars_to_write:
             description = self.Reader.get_std_name(variable)
             unit = self.Reader.get_units(variable)
             symbol = self.Reader.dataset[variable].units
             label = self.Reader.dataset[variable].long_name
-            params = {}
-            params = Parameter(description=description, variable_name=variable, symbol=symbol, unit=unit, )
+            params = Parameter(description=description, variable_name=variable, symbol=symbol, unit=unit,observed_property=label )
 
 
         return params
@@ -91,6 +111,7 @@ class Writer(object):
         # TODO iteration through variable list
         variable = self.vars_to_write[0]
 
+
         if self.tiled:
             tile_set = TileSet(self.tile_shape, self.urlTemplate)
             variable_type = self.Reader.get_type(variable)
@@ -99,13 +120,7 @@ class Writer(object):
             for tile in tile_set.get_tiles(self.tile_shape, self.Reader.dataset[variable].values):
                 count +=1
                 range = {'ranges':Range('NdArray', data_type=variable_type, axes=tile[1], shape=variable_shape, values=tile[0].flatten().tolist()).to_dict()}
-                save_covjson_range(range, str(count) +'.json' )
-
-
-
-
-
-
+                self.save_covjson_range(range, + str(count) +'.json' )
 
         axes = self.Reader.get_axis(variable)
         print('Axis Shape: ', axes)
@@ -122,9 +137,9 @@ class Writer(object):
         range = Range(range_type='NdArray',  data_type=data_type, values=values, shape= shape, variable_name=variable, axes=axes )
 
 
-
         return range
 
+    # Adapted from https://github.com/the-iea/ecem/blob/master/preprocess/ecem/util.py - letmaik
     def _save_json(self, obj, path, **kw):
         with open(path, 'w') as fp:
             print("Converting....")
@@ -137,12 +152,12 @@ class Writer(object):
     def _save_covjson(self, obj, path):
         # skip indentation of certain fields to make it more compact but still human readable
         for axis in obj['domain']['axes'].values():
-            compact(axis, 'values')
+            self.compact(axis, 'values')
         for ref in obj['domain']['referencing']:
-            no_indent(ref, 'coordinates')
+            self.no_indent(ref, 'coordinates')
         for range in obj['ranges'].values():
-            no_indent(range, 'axisNames', 'shape')
-            compact(range, 'values')
+            self.no_indent(range, 'axisNames', 'shape')
+            self.compact(range, 'values')
         self.save_json(obj, path, indent=2)
 
     def save_json(self, obj, path, **kw):
@@ -154,46 +169,24 @@ class Writer(object):
             stop = time.clock()
             print("Completed in: ", (stop - start), "seconds.")
 
+    def save_covjson_range(self, obj, path):
+        for range in obj['ranges'].values():
+            self.no_indent(range, 'axisNames', 'shape')
+            self.compact(range, 'values')
+        self.save_json(obj, path, indent=2)
 
-# Adapted from https://github.com/the-iea/ecem/blob/master/preprocess/ecem/util.py - letmaik
+    def compact(self, obj, *names):
+        for name in names:
+            obj[name] = Custom(obj[name], separators=(',', ':'))
 
-
-def save_json(obj, path, **kw):
-    with open(path, 'w') as fp:
-        print("Converting....")
-        start = time.clock()
-        jsonstr = json.dumps(obj, fp, cls=CustomEncoder, **kw)
-        fp.write(jsonstr)
-        stop = time.clock()
-        print("Completed in: ", (stop - start), "seconds.")
-
-
-def save_covjson(obj, path):
-    # skip indentation of certain fields to make it more compact but still human readable
-    for axis in obj['domain']['axes'].values():
-        compact(axis, 'values')
-    for ref in obj['domain']['referencing']:
-        no_indent(ref, 'coordinates')
-    for range in obj['ranges'].values():
-        no_indent(range, 'axisNames', 'shape')
-        compact(range, 'values')
-    save_json(obj, path, indent=2)
-
-def save_covjson_range(obj, path):
-    for range in obj['ranges'].values():
-        no_indent(range, 'axisNames', 'shape')
-        compact(range, 'values')
-    save_json(obj, path, indent=2)
+    def no_indent(self, obj, *names):
+        for name in names:
+            obj[name] = Custom(obj[name])
 
 
-def compact(obj, *names):
-    for name in names:
-        obj[name] = Custom(obj[name], separators=(',', ':'))
 
 
-def no_indent(obj, *names):
-    for name in names:
-        obj[name] = Custom(obj[name])
+
 
 
 # From http://stackoverflow.com/a/25935321
