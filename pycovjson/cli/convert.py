6aa68fc65cb08b8b67bc0f77c835cd81685c9486
Riley Williams <riley.will1@gmail.com>
2016-08-30 14:27:40 +0100

    Moved code into main

--- pycovjson/cli/convert.py
+++ pycovjson/cli/convert.py
@@ -7,20 +7,20 @@ import argparse
 
 from pycovjson.write import Writer
 
-parser = argparse.ArgumentParser(description='Convert Scientific Data Formats into CovJSON.')
-parser.add_argument('-i', '--input', dest='inputfile', help='Name of input file', required=True)
-parser.add_argument('-o', '--output', dest='outputfile', help='Name and location of output file')
-parser.add_argument('-t', '--tiled', action='store_true', help='Apply tiling')
-parser.add_argument('-s', '--shape', nargs='+', help='Tile shape, list', type=int)
-parser.add_argument('-v',dest='variable' ,help='Variable to populate coverage with')
-args = parser.parse_args()
-inputfile = args.inputfile
-outputfile = args.outputfile
-variable = args.variable
-tiled = args.tiled
-tile_shape = args.shape
 
 def main():
+    parser = argparse.ArgumentParser(description='Convert Scientific Data Formats into CovJSON.')
+    parser.add_argument('-i', '--input', dest='inputfile', help='Name of input file', required=True)
+    parser.add_argument('-o', '--output', dest='outputfile', help='Name and location of output file')
+    parser.add_argument('-t', '--tiled', action='store_true', help='Apply tiling')
+    parser.add_argument('-s', '--shape', nargs='+', help='Tile shape, list', type=int)
+    parser.add_argument('-v', dest='variable', help='Variable to populate coverage with')
+    args = parser.parse_args()
+    inputfile = args.inputfile
+    outputfile = args.outputfile
+    variable = args.variable
+    tiled = args.tiled
+    tile_shape = args.shape
     Writer(outputfile, inputfile, [variable], tiled=tiled, tile_shape=tile_shape).write()
 
 if __name__ == '__main__':
