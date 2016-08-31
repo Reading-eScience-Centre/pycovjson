1e8aa2686f0c01f22e161fb6288c77e8c2f9b368
Riley Williams <riley.will1@gmail.com>
2016-08-31 11:04:08 +0100

    Added viewer.py

---
+++ pycovjson/cli/viewer.py
@@ -0,0 +1,33 @@
+"""
+Pycovjson - Command line viewer
+Author: rileywilliams
+Version: 0.1.0
+TODO - Add support for other formats and more customisation
+"""
+import argparse
+
+
+from pycovjson.readNetCDFOOP import NetCDFReader as Reader
+
+
+def main():
+    parser = argparse.ArgumentParser(description='View Scientific Data files.')
+    parser.add_argument('-i', '--input', dest='inputfile', help='Name of input file', required=True)
+
+    parser.add_argument('-v', '--variables,', dest='variables', help='Display variables', action='store_true')
+
+    args = parser.parse_args()
+    inputfile = args.inputfile
+    variables = args.variables
+    reader = Reader(inputfile)
+    Reader(inputfile).print()
+    reader.print()
+    if variables:
+        reader.get_vars_with_long_name(inputfile)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
