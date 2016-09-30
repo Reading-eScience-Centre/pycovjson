506c224e1295bbbfee5f22909ba32610cecaf8bc
Riley Williams <riley.will1@gmail.com>
2016-09-30 17:44:48 +0100

    Updated conf.py - Added path to pycovjson to allow autodoc to read docstrings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,12 +21,13 @@ from recommonmark.parser import CommonMarkParser
 source_parsers = {
     '.md': CommonMarkParser,
 }
+autoclass_content = 'both'
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 
 # -- General configuration ------------------------------------------------
