5c2957be102360775eae96f2ecc568fc7649c2df
Lewis John McGibbney <lewis.mcgibbney@gmail.com>
2017-07-11 12:26:02 -0700

    ISSUE-35 Add flag to pycovjson-convert to pipe data into MongoDB

--- pycovjson/cli/convert.py
+++ pycovjson/cli/convert.py
@@ -19,6 +19,7 @@ def main():
     :argument -v: Which variable to populate coverage with.
     :argument -s: [tile shape]: Tile shape.
     :argument -n: Use interactive mode.
+    :argument -u: MongoDB URL
 
 
     """
@@ -35,6 +36,7 @@ def main():
     parser.add_argument('-v', dest='variable',
                         help='Variable to populate coverage with', required=True)
     parser.add_argument('-n', '--interactive', action='store_true', help='Enter interactive mode')
+    parser.add_argument('-u', '--endpoint_url', dest='endpoint_url', nargs=1, help='MongoDB endpoint for CovJSON persistence')
     args = parser.parse_args()
     inputfile = args.inputfile
     outputfile = args.outputfile
@@ -42,6 +44,7 @@ def main():
     tiled = args.tiled
     tile_shape = args.shape
     interactive = args.interactive
+    endpoint_url = args.endpoint_url
 
     if interactive:
         axis = input('Which Axis?', Reader.get_axis(variable))
@@ -59,8 +62,9 @@ def main():
     if outputfile == None:
         outputfile = outputfile.default
 
-    Writer(outputfile, inputfile, [variable],
-           tiled=tiled, tile_shape=tile_shape).write()
+    if endpoint_url is not None:
+        Writer(outputfile, inputfile, [variable],
+               tiled=tiled, tile_shape=tile_shape, endpoint_url=endpoint_url).write()
 
 if __name__ == '__main__':
     main()
